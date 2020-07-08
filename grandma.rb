a9633fdd344312222bc2c86ec1d592ce58dd5b06
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-08 02:01:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == upcase
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  # if phrase == "I LOVE YOU GRANDMA!"
-  #   "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   end
