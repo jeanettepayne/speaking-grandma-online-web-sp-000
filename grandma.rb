d747b71f214a531431cb08206fec2b161e3e66a0
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-08 02:07:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
+  elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
-  # elsif phrase == "Hi Nana, how are you?"
-  #   "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
   elsif "WHAT DID YOU EAT TODAY?"
